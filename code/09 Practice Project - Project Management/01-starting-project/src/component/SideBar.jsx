14ea37c64e097497972f801dfba5054222590dc0
rpyimuk <prijmuk1711@gmail.com>
2024-05-15 23:12:34 +0300

    delete by name working

--- code/09 Practice Project - Project Management/01-starting-project/src/component/SideBar.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/component/SideBar.jsx
@@ -9,15 +9,37 @@ import {useState} from "react";
 //     }
 // }
 
+const emptyObj = {
+    // id: ++idCounter,
+    name: "",
+    description: "",
+    date: null,
+    tasks: []
+}
+
+const fullObj = {
+    // id: ++idCounter,
+    name: "name",
+    description: "description",
+    date: new Date().toDateString(),
+    tasks: [{task: 'one'}]
+}
+
+// let idCounter = 0;
+
 export default function SideBar() {
+
     const [userInput, setUserInput] = useState({
+        id: 0,
         name: "",
+        description: "",
+        date: new Date().toDateString(),
         tasks: []
     });
-    const [project, setProject] = useState([{name: 'test', tasks: [{task: 'one'}]}]);
+    const [project, setProject] = useState([]);
 
     function handleChange(identifier, newValue) {
-        setUserInput( (prevState) => {
+        setUserInput((prevState) => {
                 return {...prevState, [identifier]: newValue}
             }
         )
@@ -27,28 +49,69 @@ export default function SideBar() {
         setProject((prev) => {
             let oldAr = [...prev]
             oldAr.push(userInput)
+            setUserInput(emptyObj)
             return oldAr
         });
     }
 
-    return (
-        <div>
+    function deleteProject(nameForDelete) {
+    //     let con ;
+    // project.map((item, index) => {
+    //         if (item.name === nameForDelete) {
+                console.log(project.filter(proj => proj.name !== nameForDelete))
+    //             con = project.splice(index, 1)
+    //         }
+    //     })
+    //
+    //     setProject((prev) => {
+    //         prev.map((item, index) => {
+    //             if (item.name === nameForDelete) {
+    //                 console.log('test')
+    //                 console.log(prev.splice(index, 1))
+    //             }
+    //         })
+    //
+    //     })
 
+
+
+        setProject((oldValues) => {
+            return oldValues.filter(proj => proj.name !== nameForDelete)
+        })
+    }
+
+    return (
+        <section>
+            <label>Name: </label>
             <input
                 type="text"
                 required
                 value={userInput.name}
                 onChange={(event) => handleChange("name", event.target.value)}/>
 
+            <label>Description: </label>
+            <input
+                type="text"
+                required
+                value={userInput.description}
+                onChange={(event) => handleChange("description", event.target.value)}/>
+
             <div className="actions">
                 <button onClick={addProject}>Add Project</button>
             </div>
 
             <div>
-            {project.map((item, index) => (
-                    <div key={index}>{item.name}</div>
+                {project.map((item, index) => (
+                    <div key={index}>
+                        <li key={index}>
+                            <span>Name: {item.name};</span>
+                            <span>Date: {item.date};</span>
+                            <span>Description: {item.description};</span>
+                            <button onClick={() => deleteProject(item.name)}>Delete</button>
+                        </li>
+                    </div>
                 ))}
             </div>
-        </div>
+        </section>
     )
 }
